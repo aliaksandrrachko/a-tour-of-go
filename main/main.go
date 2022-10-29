b47abb2d1c12a2e77381d4ae91f53bc7b9616028
aliaksandrrachko <rachko.a.ch@gmail.com>
2022-10-29 16:08:11 +0300

    feature: finished more types
    add .gitingnore

--- main/main.go
+++ main/main.go
@@ -7,11 +7,13 @@ func main() {
 	fmt.Println("-- Basics: Packages, variables, and funcions.")
 	fmt.Println("=======================================================")
 	pakcagesVariablesFunctions()
-	fmt.Println("\n\n\n\n")
+	fmt.Printf("\n\n\n\n")
 
 	fmt.Println("=======================================================")
 	fmt.Println("-- Basics: Flow control statemsnts: for, if, else, witch and defer")
 	fmt.Println("=======================================================")
 	flowControlStatments()
-	fmt.Println("\n\n\n\n")
+	fmt.Printf("\n\n\n\n")
+
+	moreTypes()
 }
