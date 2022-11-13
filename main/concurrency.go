4e81a9074ed3a049acf9f7e71078e71ab8a30c6b
aliaksandrrachko <rachko.a.ch@gmail.com>
2022-11-13 17:16:55 +0300

    stil study concurrency

--- main/concurrency.go
+++ main/concurrency.go
@@ -1,6 +1,42 @@
 package main
 
-func concurrency() {
-	// Goroutines
+import (
+	"fmt"
+	"time"
+)
 
+func concurrency() {
+	// Goroutines - is a lightweight thread managed by the Go runtime
+	go say("world")
+	say("hello")
+
+	// Channels
+	intSliceToSum := []int{
+		7, 2, 8, -9, 4, 0, -280, 3,
+		-200, 615, -165, 48, 6, 15,
+		-5, 6, 5, 54, 6, 1, 32, 2,
+	}
+
+	sumChannel := make(chan int)
+	go sum(intSliceToSum[:len(intSliceToSum)/2], sumChannel)
+	go sum(intSliceToSum[len(intSliceToSum)/2:], sumChannel)
+	x, y := <-sumChannel, <-sumChannel // receive from c
+	fmt.Println(x, y, x+y)
+
+	// Buffered Channels
+}
+
+func say(s string) {
+	for i := 0; i < 5; i++ {
+		time.Sleep(100 * time.Microsecond)
+		fmt.Println(s)
+	}
+}
+
+func sum(s []int, c chan int) {
+	sum := 0
+	for _, v := range s {
+		sum += v
+	}
+	c <- sum // send sum to c
 }
